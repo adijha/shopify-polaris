46ea88857ba26ce0f0535b09843d05592e995d10
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-11-15 17:05:48 +0530

    history

--- src/components/History.js
+++ src/components/History.js
@@ -1,39 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { DescriptionList } from '@shopify/polaris';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { DescriptionList } from "@shopify/polaris";
+import axios from "axios";
 
 export default function History(props) {
-	const [ history, setHistory ] = useState([]);
+  const [history, setHistory] = useState([]);
 
-	const smsHistory = () => {
-		axios.get('/api/history/').then((res) => {
-			console.log('history', res.data);
-			setHistory(res.data);
-		});
-	};
+  const smsHistory = () => {
+    axios.get("/api/history/").then(res => {
+      console.log("history", res.data);
+      setHistory(res.data);
+    });
+  };
 
-	useEffect(() => {
-		smsHistory();
-	}, []);
+  useEffect(() => {
+    smsHistory();
+  }, []);
 
-	return (
-		<DescriptionList
-			items={[
-				{
-					term: 'Logistics',
-					description:
-						'The management of products or other resources as they travel between a point of origin and a destination.'
-				},
-				{
-					term: 'Sole proprietorship',
-					description: 'A business structure where a single individual both owns and runs the company.'
-				},
-				{
-					term: 'Discount code',
-					description:
-						'A series of numbers and/or letters that an online shopper may enter at checkout to get a discount or special offer.'
-				}
-			]}
-		/>
-	);
+  return <DescriptionList items={history} />;
 }
