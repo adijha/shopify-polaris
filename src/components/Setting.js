c57e93733bac5b4e79e31bc9f1d8ac08ed404d9b
Aditya Kumar Jha <adityajha126@gmail.com>
2019-11-15 12:32:05 +0530

    settings and edit template ui completed

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,137 +1,129 @@
-import React, { Fragment } from 'react';
-import { Button } from '@shopify/polaris';
-import Themee from './Themee';
-import Themes from './Themes';
+import React, { useCallback, useRef, useState, Fragment } from 'react';
+import { Card, Layout, Heading, Button } from '@shopify/polaris';
 
+import Checkbox from './Checkbox';
 
+import DefaultTextField from './DefaultTextField';
 
 export default function Settings() {
 	return (
 		<Fragment>
-			<Themes />
-			{/* <div style={{ display: 'flex', margin: '0' }}>
-				<div style={{ width: '40rem', marginRight: '1rem' }}>
-					<Card>
-						<div style={{ padding: '3rem', width: '35rem' }}>
-							<DefaultTextField label="Admin Phone No." type="text" maxLength="10" />
-						</div>
-					</Card>
-				</div>
-				<div style={{ width: '40rem', marginLeft: '1rem' }}>
-					<Card>
-						<div style={{ padding: '3rem', width: '35rem' }}>
-							<DefaultTextField elpText label="Sender ID" type="text" maxLength="6" />
-						</div>
-					</Card>
-				</div>
-			</div> */}
-
-			{/* <br /> */}
-
-			{/* <div>
-				<div style={{ marginBottom: '5px' }}>
-					<Heading size="large">SMS Notification Prefrence</Heading> */}
-			{/* </div>
-				<Card> */}
-			{/* <div style={{ padding: '2rem' }}>
-						<DisplayText size="small">Orders</DisplayText>
-						<hr />
-
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Create </Heading>
+			<div>
+				<Layout>
+					<Layout.AnnotatedSection
+						title="Admin Phone No."
+						description="Admin will be notify on this no. by selecting Notify Admin."
+					>
+						<Card sectioned>
+							<div style={{ padding: '3rem' }}>
+								<DefaultTextField label="Admin Phone No." type="text" maxLength="10" />
 							</div>
+						</Card>
+					</Layout.AnnotatedSection>
+					<Layout.AnnotatedSection
+						title="Sender ID"
+						description="Sender ID is the name or number which appears on the mobile phone as the sender of a SMS. Sender ID will be maximum of 6 Characters."
+					>
+						<Card sectioned>
+							<div style={{ padding: '3rem' }}>
+								<DefaultTextField elpText label="Sender ID" type="text" maxLength="6" />
+							</div>
+						</Card>
+					</Layout.AnnotatedSection>
+					<Layout.AnnotatedSection
+						title="Notification Prefrence"
+						description="Admin and Customer will be notified according to by selecting Notify Admin."
+					>
+						<Card sectioned>
+							<div style={{ padding: '2rem' }}>
+								<p style={{ fontSize: '17px' }}>Orders</p>
+								<hr />
 
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Cancelled </Heading>
-							</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Create </Heading>
+									</div>
 
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Fulfilled </Heading>
-							</div>
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" hell="orders/create" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Cancelled </Heading>
+									</div>
 
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Partially Fulfilled </Heading>
-							</div>
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Fulfilled </Heading>
+									</div>
 
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Paid </Heading>
-							</div>
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Partially Fulfilled </Heading>
+									</div>
 
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Paid </Heading>
+									</div>
+
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+
+								<br />
+								<p style={{ fontSize: '17px' }}>Customers Account</p>
+								<hr />
+
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Create </Heading>
+									</div>
+
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
+								<br />
+								<p style={{ fontSize: '17px' }}>Refund</p>
+								<hr />
+
+								<div style={{ display: 'flex' }}>
+									<div style={{ marginRight: '1rem', width: '15rem' }}>
+										<Heading>Create </Heading>
+									</div>
+
+									<div style={{ width: '15rem' }}>
+										<Checkbox label="Notify Customer" />
+									</div>
+									<Checkbox label="Notify Admin" />
+								</div>
 							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-
-						<br />
-						<DisplayText size="small">Customers Account</DisplayText>
-						<hr />
-
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Create </Heading>
-							</div>
-
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-						<br />
-						<DisplayText size="small">Refund</DisplayText>
-						<hr />
-
-						<div style={{ display: 'flex' }}>
-							<div style={{ marginRight: '1rem', width: '20rem' }}>
-								<Heading>Create </Heading>
-							</div>
-
-							<div style={{ width: '24rem' }}>
-								<Checkbox label="Notify Customer" />
-							</div>
-							<Checkbox label="Notify Admin" />
-						</div>
-					</div> */}
-			{/* </Card>
-			</div> */}
-
-			{/* <TextContainer>
-				<DisplayText size="small">What is Sender ID?</DisplayText>
-				<p>
-					Manage your Shopify store on-the-go with real-time notifications, access to your dashboard, and order
-					management, all from your smartphone.
-				</p>
-				<p>
-					Shopify POS is the fastest and easiest way to start accepting Visa, Mastercard, American Express, and Discover
-					right from your smartphone or tablet.
-				</p>
-			</TextContainer> */}
-			<br/>
+						</Card>
+					</Layout.AnnotatedSection>
+				</Layout>
+			</div>
+			<br />
 			<div style={{ textAlign: 'right' }} onClick={() => {}}>
 				<Button primary>Save</Button>
 			</div>
--- src/components/Template.js
+++ src/components/Template.js
@@ -1,110 +1,136 @@
 import React, { Fragment, useState, useCallback, useEffect } from 'react';
-import { Button } from '@shopify/polaris';
+import { Button, Card } from '@shopify/polaris';
 
 import axios from 'axios';
 
 export default function Template(props) {
-	let defaultTopic = 'orders/create';
-	let defaultTopicVariable = 'name vendor price order_id title';
+	let defaultTopic = {
+		topic: 'orders/create',
+		topicVariables: 'name vendor price order_id title'
+	};
 
-	const [ topic, setTopic ] = useState(defaultTopic);
+	const [ topics, setTopics ] = useState(defaultTopic);
 
-	const [ topicVariables, setTopicVariables ] = useState(defaultTopicVariable);
-	// const sendTemplate = (tempObj) => {
-	// 	if (tempObj.customer && tempObj.admin && tempObj.topic) {
-	// 		console.log(tempObj);
-	// 		axios.post('/api/template', tempObj).then((res) => console.log(res)).catch((err) => console.error(err));
-	// 	} else {
-	// 		console.log(" couldn't know what happened");
-	// 	}
-	// };
+	const sendTemplate = (tempObj) => {
+		if (tempObj.customer && tempObj.admin && tempObj.topic) {
+			console.log(tempObj);
+			axios.post('/api/template', tempObj).then((res) => console.log(res)).catch((err) => console.error(err));
+		} else {
+			console.log(" couldn't know what happened");
+		}
+	};
 
-	// let tempObj = {
-	// 	topic: topic,
-	// 	customer: '',
-	// 	admin: ''
-	// };
-	// let finObj = {};
+	let tempObj = {
+		topic: topics.topic,
+		customer: '',
+		admin: ''
+	};
+	let finObj = {};
 
-	// const saveTemplete = () => {
-	// 	convertData({
-	// 		textareaId: 'inputTextArea-customer',
-	// 		audience: 'customer'
-	// 	});
-	// 	convertData({
-	// 		textareaId: 'inputTextArea-admin',
-	// 		audience: 'admin'
-	// 	});
-	// };
+	const saveTemplete = () => {
+		convertData({
+			textareaId: 'inputTextArea-customer',
+			audience: 'customer'
+		});
+		convertData({
+			textareaId: 'inputTextArea-admin',
+			audience: 'admin'
+		});
+	};
 
-	// const convertData = (param) => {
-	// 	let first = param.textareaId;
+	const convertData = (param) => {
+		let first = param.textareaId;
 
-	// 	let inputTextArea = document.getElementById(first);
+		let inputTextArea = document.getElementById(first);
 
-	// 	let strLength = 0;
-	// 	let luna = document.getElementById(first).className;
-	// 	var inputData = document.getElementById(first).value;
-	// 	if (inputData != '' && inputData != null) {
-	// 		inputData = inputData.replace(/(^\s*)|(\s*$)/gi, '');
-	// 		inputData = inputData.replace(/[ ]{2,}/gi, ' ');
-	// 		inputData = inputData.replace(/\n /, '\n');
+		let strLength = 0;
+		let luna = document.getElementById(first).className;
+		var inputData = document.getElementById(first).value;
+		if (inputData != '' && inputData != null) {
+			inputData = inputData.replace(/(^\s*)|(\s*$)/gi, '');
+			inputData = inputData.replace(/[ ]{2,}/gi, ' ');
+			inputData = inputData.replace(/\n /, '\n');
 
-	// 		for (let i = 0; i < inputData.length; i++) {
-	// 			inputData = inputData.replace(' ', '%20');
-	// 			inputData = inputData.replace('(', '${');
-	// 			inputData = inputData.replace(')', '}');
-	// 			inputData = inputData.replace('\n', '%0A');
-	// 		}
+			for (let i = 0; i < inputData.length; i++) {
+				inputData = inputData.replace(' ', '%20');
+				inputData = inputData.replace('(', '${');
+				inputData = inputData.replace(')', '}');
+				inputData = inputData.replace('\n', '%0A');
+			}
 
-	// 		if (luna.indexOf('is-invalid') > -1) {
-	// 			strLength = luna.split(' ').length;
-	// 			for (let i = 0; i < strLength; i++) {
-	// 				luna = luna.replace('is-invalid', '');
-	// 			}
+			if (luna.indexOf('is-invalid') > -1) {
+				strLength = luna.split(' ').length;
+				for (let i = 0; i < strLength; i++) {
+					luna = luna.replace('is-invalid', '');
+				}
 
-	// 			inputTextArea.className = luna;
-	// 		}
-	// 	} else {
-	// 		luna = document.getElementById(first).className;
-	// 		luna = luna.concat(' ');
-	// 		luna = luna.concat('is-invalid');
-	// 		document.getElementById(first).className = luna;
-	// 	}
-	// 	showOutput(inputData, param.audience);
-	// };
+				inputTextArea.className = luna;
+			}
+			myFunction();
+		} else {
+			luna = document.getElementById(first).className;
+			luna = luna.concat(' ');
+			luna = luna.concat('is-invalid');
+			document.getElementById(first).className = luna;
+		}
+		showOutput(inputData, param.audience);
+	};
 
-	// const showOutput = (parameter, audience) => {
-	// 	parameter = `\`${parameter}\``;
+	const showOutput = (parameter, audience) => {
+		parameter = `\`${parameter}\``;
 
-	// 	if (audience == 'customer') {
-	// 		tempObj.customer = parameter;
-	// 	} else if (audience == 'admin') {
-	// 		tempObj.admin = parameter;
-	// 	} else {
-	// 		console.log('something went wrong ');
-	// 	}
-	// 	// console.log(tempObj);
-	// 	sendTemplate(tempObj);
-	// };
-	// const showState = () => {
-	// 	console.log(topic);
-	// };
+		if (audience == 'customer') {
+			tempObj.customer = parameter;
+		} else if (audience == 'admin') {
+			tempObj.admin = parameter;
+		} else {
+			console.log('something went wrong ');
+		}
+		sendTemplate(tempObj);
+	};
 
 	const topicHandler = (params) => {
 		const selectedElement = params.target;
 		const selectedValue = selectedElement.innerHTML;
-		console.log(selectedValue);
-		setTopic(selectedValue);
+		switch (selectedValue) {
+			case 'orders/create':
+				setTopics({
+					...topics,
+					topic: selectedValue,
+					topicVariables: 'name  vendor  price order_id title '
+				});
+				break;
+			case 'orders/cancelled':
+				setTopics({
+					...topics,
+					topic: selectedValue,
+					topicVariables: 'name vendor price order_id title cancel_reason'
+				});
+				break;
+			case 'orders/fulfilled':
+				setTopics({
+					...topics,
+					topic: selectedValue,
+					topicVariables: 'name vendor price order_id title fulfillment_status order_status_url'
+				});
+				break;
+			case 'orders/partially_fulfilled':
+				setTopics({
+					...topics,
+					topic: selectedValue,
+					topicVariables: 'name vendor price order_id title fulfillment_status order_status_url'
+				});
+				break;
+			case 'customers/create':
+				setTopics({ ...topics, topic: selectedValue, topicVariables: 'name email phone' });
+				break;
+			case 'refunds/create':
+				setTopics({ ...topics, topic: selectedValue, topicVariables: 'price order_id title' });
+				break;
 
-		// switch (selectedValue) {
-		// 	case 'orders/cancelled':
-				setTopicVariables('name vendor price order_id title cancel_reason');
-		// 		break;
-
-		// 	default:
-		// 		break;
-		// }
+			default:
+				break;
+		}
 
 		let selectedElementClass = selectedElement.className;
 
@@ -113,9 +139,7 @@ export default function Template(props) {
 		} else {
 			let firstElement = document.getElementsByClassName('butti');
 			if (firstElement) {
-				// console.log(firstElement);
 				let first = firstElement[0];
-				// console.log(first);
 				first.className = 'butt ';
 			}
 
@@ -130,22 +154,20 @@ export default function Template(props) {
 		x.className = 'show';
 		setTimeout(function() {
 			x.className = x.className.replace('show', '');
-		}, 3000);
+		}, 2000);
 	}
 
 	return (
 		<Fragment>
 			<h2>Select Topic</h2>
 
-			<div id="snackbar">Template Updated for {topic}</div>
+			<div id="snackbar">Template Updated for {topics.topic} &nbsp;👍 </div>
 			<br />
 			<div className="bog col-md-12">
 				<div className="butti " onClick={topicHandler}>
 					orders/create
 				</div>
-				{/* <div className="butt " onClick={topicHandler}>
-					orders/create
-				</div> */}
+
 				<div className="butt " onClick={topicHandler}>
 					orders/cancelled
 				</div>
@@ -166,78 +188,80 @@ export default function Template(props) {
 			<div className="row ">
 				<div className="col-sm-6">
 					<div className="card mt-5 shadow">
-						<div className="card-body">
-							<h5>SMS Template Rules</h5>
+						<Card>
+							<div style={{ padding: '2rem' }}>
+								<h5>SMS Template Rules</h5>
 
-							<div style={{ textAlign: 'left', fontSize: '15px' }}>
-								All the variables enclosed in "( )" will be replaced by actual values.
-								<br />
-								SMS length linit is 70 characters.
-								<br />
-								Enclose every variables with "( )"
-								<br />
-								<br />
-								<p
-									style={{
-										fontSize: '16px',
-										fontWeight: '500',
-										color: '#444444'
-									}}
-								>
-									Available Variables for {topic}
-								</p>
-								<div id="places" style={{ width: '80px' }}>
-									{topicVariables}
+								<div style={{ textAlign: 'left', fontSize: '15px' }}>
+									All the variables enclosed in "( )" will be replaced by actual values.
+									<br />
+									SMS length linit is 70 characters.
+									<br />
+									Enclose every variables with "( )"
+									<br />
+									<br />
+									<p
+										style={{
+											fontSize: '16px',
+											fontWeight: '500',
+											color: '#444444'
+										}}
+									>
+										Available Variables for {topics.topic}
+									</p>
+									<div id="places" style={{ wordSpacing: '50px', width: '120px' }}>
+										{topics.topicVariables}
+									</div>
 								</div>
 							</div>
-						</div>
+						</Card>
 					</div>
 				</div>
 
 				<div className="col-sm-6">
 					<div className="card text-center mt-5 shadow">
-						<div className="card-body">
-							<h5 className="card-title">Customer Message templete</h5>
+						<Card>
+							<div style={{ padding: '2rem' }}>
+								<h5 className="card-title">Customer Message templete</h5>
 
-							{/* <TemplateMsg /> */}
+								<textarea
+									className="form-control"
+									id="inputTextArea-customer"
+									placeholder="Enter customer message here"
+									rows={5}
+									required
+									autoFocus
+									defaultValue={''}
+									style={{ fontSize: '15px' }}
+								/>
 
-							<textarea
-								className="form-control"
-								id="inputTextArea-customer"
-								placeholder="Enter customer message here"
-								rows={5}
-								required
-								autoFocus
-								defaultValue={''}
-								style={{ fontSize: '15px' }}
-							/>
+								<hr />
+								<h5 className="card-title">Admin Message Templete</h5>
 
-							<hr />
-							<h5 className="card-title">Admin Message Templete</h5>
+								<textarea
+									className="form-control"
+									id="inputTextArea-admin"
+									placeholder="Enter admin message here"
+									rows={5}
+									required
+									autoFocus
+									defaultValue={''}
+									style={{ fontSize: '15px' }}
+								/>
+								<div className="invalid-feedback">Please enter Data in the textarea.</div>
 
-							<textarea
-								className="form-control"
-								id="inputTextArea-admin"
-								placeholder="Enter admin message here"
-								rows={5}
-								required
-								autoFocus
-								defaultValue={''}
-								style={{ fontSize: '15px' }}
-							/>
-							<div className="invalid-feedback">Please enter Data in the textarea.</div>
-
-							<br />
-							<div style={{ textAlign: 'center' }} onClick={() => {}}>
+								<br />
+								{/* <div style={{ textAlign: 'center' }} onClick={() => {}}>
 								<Button primary>Show State</Button>
 							</div>
 							<div style={{ textAlign: 'center' }} onClick={myFunction}>
 								<Button primary>Save Template</Button>
-							</div>
-							{/* <div style={{ textAlign: 'center' }} onClick={saveTemplete}>
-								<Button primary>Save Template</Button>
 							</div> */}
-						</div>
+								<div style={{ textAlign: 'center' }} onClick={saveTemplete}>
+									<Button primary>Save Template</Button>
+								</div>
+							</div>
+						</Card>
 					</div>
 				</div>
 			</div>
